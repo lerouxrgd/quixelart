42aa0d0cb6b9eed9d4e5c39c355f17255d3bee86
Romain Leroux <romain@leroux.dev>
2020-08-31 02:13:22 +0200

    Don't recreate image on each view

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::path::PathBuf;
 
+use iced::image::Handle as ImageHandle;
+use iced::{button, scrollable, slider, text_input};
 use iced::{
-    button, scrollable, slider, text_input, Button, Checkbox, Color, Column, Container, Element,
-    HorizontalAlignment, Image, Length, Radio, Row, Sandbox, Scrollable, Settings, Slider, Space,
-    Text, TextInput,
+    Button, Checkbox, Color, Column, Container, Element, HorizontalAlignment, Image, Length, Radio,
+    Row, Sandbox, Scrollable, Settings, Slider, Space, Text, TextInput,
 };
 use subprocess::Exec;
 
@@ -11,21 +12,23 @@ struct Easel {
     scroll: scrollable::State,
     src_btn: button::State,
     src_img: Option<PathBuf>,
+    img_handle: ImageHandle,
 }
 
 #[derive(Debug, Clone)]
-enum Msg {
+enum Event {
     SourcePressed,
 }
 
 impl Sandbox for Easel {
-    type Message = Msg;
+    type Message = Event;
 
     fn new() -> Self {
         Self {
             scroll: scrollable::State::new(),
             src_btn: button::State::new(),
             src_img: None,
+            img_handle: ImageHandle::from_memory(vec![]),
         }
     }
 
@@ -33,37 +36,47 @@ impl Sandbox for Easel {
         "QuixelArt".into()
     }
 
-    fn update(&mut self, evt: Msg) {
+    fn update(&mut self, evt: Event) {
         match evt {
-            Msg::SourcePressed => {
+            Event::SourcePressed => {
                 self.src_img = match nfd2::open_file_dialog(None, None).unwrap() {
                     nfd2::Response::Okay(file_path) => Some(file_path),
                     nfd2::Response::OkayMultiple(_) | nfd2::Response::Cancel => None,
                 };
+
+                if let Some(path_buf) = &self.src_img {
+                    let img_bytes = make_img(&path_buf.as_path().to_string_lossy());
+                    self.img_handle = ImageHandle::from_memory(img_bytes);
+                }
             }
         }
     }
 
-    fn view(&mut self) -> Element<Msg> {
-        let img_bytes = match &self.src_img {
-            Some(path_buf) => make_img(&path_buf.as_path().to_string_lossy()),
-            None => vec![],
-        };
-        let img = iced::image::Handle::from_memory(img_bytes);
+    fn view(&mut self) -> Element<Event> {
+        // let mut content = Row::new();
+        let mut content = Column::new();
 
-        let content = Column::new()
+        let a = Column::new()
             .spacing(20)
-            .push(Text::new("Image").size(50))
-            .push(Container::new(Image::new(img)))
+            .width(Length::Fill)
+            .push(Text::new("Image").size(34))
+            .push(Container::new(Image::new(self.img_handle.clone())))
             .push(
                 Button::new(
                     &mut self.src_btn,
-                    Text::new("Select image").horizontal_alignment(HorizontalAlignment::Center),
+                    Text::new("Choose image").horizontal_alignment(HorizontalAlignment::Center),
                 )
                 .padding(12)
                 .min_width(100)
-                .on_press(Msg::SourcePressed),
+                .on_press(Event::SourcePressed),
             );
+
+        let b = Column::new()
+            .spacing(20)
+            .width(Length::Fill)
+            .push(Text::new("Placeholder").size(18));
+
+        content = content.push(a).push(b);
 
         let scrollable = Scrollable::new(&mut self.scroll)
             .push(Container::new(content).width(Length::Fill).center_x());
@@ -76,7 +89,8 @@ impl Sandbox for Easel {
 }
 
 fn make_img(src: &str) -> Vec<u8> {
-    (Exec::cmd("convert")
+    (Exec::cmd("magick")
+        .arg("convert")
         .arg(src)
         .arg("-resize")
         .arg("20%")
@@ -88,7 +102,8 @@ fn make_img(src: &str) -> Vec<u8> {
             .arg("-kmeans")
             .arg("32")
             .arg("-")
-        | Exec::cmd("convert")
+        | Exec::cmd("magick")
+            .arg("convert")
             .arg("-")
             .arg("-filter")
             .arg("point")
