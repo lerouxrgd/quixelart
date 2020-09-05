5fb58c1a806f0935d67002f19119f57ad66be88c
Romain Leroux <romain@leroux.dev>
2020-09-05 21:13:19 +0200

    More sliders

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,10 @@ mod style;
 use std::path::PathBuf;
 
 use iced::image::Handle as ImageHandle;
-use iced::{button, scrollable, slider, text_input};
+use iced::{button, scrollable, slider};
 use iced::{
     Align, Button, Checkbox, Color, Column, Container, Element, Font, HorizontalAlignment, Image,
-    Length, Radio, Row, Sandbox, Scrollable, Settings, Slider, Space, Text, TextInput,
-    VerticalAlignment,
+    Length, Radio, Row, Sandbox, Scrollable, Settings, Slider, Space, Text, VerticalAlignment,
 };
 use subprocess::Exec;
 
@@ -27,16 +26,28 @@ struct Easel {
     src_button: button::State,
     img_path: Option<PathBuf>,
     img_handle: ImageHandle,
-    resize_slider: slider::State,
-    resize_val: u8,
+    pixelize_slider: slider::State,
+    pixelize: u8,
+    kcolors_slider: slider::State,
+    kcolors: u8,
+    level_black_slider: slider::State,
+    level_black: u8,
+    level_white_slider: slider::State,
+    level_white: u8,
 }
 
 #[derive(Debug, Clone)]
 enum Event {
     SourcePressed,
     ThemeChanged(style::Theme),
-    SliderResizeChanged(u8),
-    SliderResizeReleased,
+    SliderPixelizeChanged(u8),
+    SliderPixelizeReleased,
+    SliderKcolorsChanged(u8),
+    SliderKcolorsReleased,
+    SliderLevelBlackChanged(u8),
+    SliderLevelBlackReleased,
+    SliderLevelWhiteChanged(u8),
+    SliderLevelWhiteReleased,
 }
 
 impl Sandbox for Easel {
@@ -49,8 +60,14 @@ impl Sandbox for Easel {
             src_button: button::State::new(),
             img_path: None,
             img_handle: ImageHandle::from_memory(vec![]),
-            resize_slider: slider::State::new(),
-            resize_val: 80,
+            pixelize_slider: slider::State::new(),
+            pixelize: 80,
+            kcolors_slider: slider::State::new(),
+            kcolors: 32,
+            level_black_slider: slider::State::new(),
+            level_black: 10,
+            level_white_slider: slider::State::new(),
+            level_white: 80,
         }
     }
 
@@ -70,18 +87,38 @@ impl Sandbox for Easel {
                 };
                 self.make_img();
             }
-            Event::SliderResizeChanged(resize_val) => {
-                self.resize_val = resize_val;
+            Event::SliderPixelizeChanged(pixelize) => {
+                self.pixelize = pixelize;
             }
-            Event::SliderResizeReleased => {
+            Event::SliderPixelizeReleased => {
+                self.make_img();
+            }
+            Event::SliderKcolorsChanged(kcolors) => {
+                self.kcolors = kcolors;
+            }
+            Event::SliderKcolorsReleased => {
+                self.make_img();
+            }
+            Event::SliderLevelBlackChanged(level_black) => {
+                self.level_black = level_black;
+            }
+            Event::SliderLevelBlackReleased => {
+                self.make_img();
+            }
+            Event::SliderLevelWhiteChanged(level_white) => {
+                self.level_white = level_white;
+            }
+            Event::SliderLevelWhiteReleased => {
                 self.make_img();
             }
         }
     }
 
     fn view(&mut self) -> Element<Event> {
+        const PADDING: u16 = 14;
+
         let choose_img = Button::new(&mut self.src_button, Text::new("Choose image"))
-            .padding(14)
+            .padding(PADDING)
             .on_press(Event::SourcePressed)
             .style(self.theme);
 
@@ -110,41 +147,113 @@ impl Sandbox for Easel {
             );
 
         let header = Row::new()
-            .padding(14)
+            .padding(PADDING)
             .push(choose_img)
             .push(Space::with_width(Length::Fill))
             .push(choose_theme);
 
-        let image = Container::new(Image::new(self.img_handle.clone())).padding(14);
+        let image = Container::new(Image::new(self.img_handle.clone())).padding(PADDING);
 
-        let resize = Row::new()
-            .padding(14)
+        let name_width = 120;
+        let val_width = 50;
+
+        let pixelize = Row::new()
+            .padding(PADDING)
             .spacing(10)
-            .push(Text::new("Pixelization").width(Length::Units(120)))
+            .push(Text::new("Pixelize").width(Length::Units(name_width)))
             .push(
                 Slider::new(
-                    &mut self.resize_slider,
+                    &mut self.pixelize_slider,
                     0..=99,
-                    self.resize_val,
-                    Event::SliderResizeChanged,
+                    self.pixelize,
+                    Event::SliderPixelizeChanged,
                 )
-                .on_release(Event::SliderResizeReleased)
+                .on_release(Event::SliderPixelizeReleased)
                 .width(Length::Fill)
                 .style(self.theme),
             )
             .push(
-                Text::new(&format!("{} %", self.resize_val))
-                    .width(Length::Units(50))
+                Text::new(&format!("{} %", self.pixelize))
+                    .width(Length::Units(val_width))
                     .font(FONT_PIX_L),
             );
 
-        let content = Column::new()
+        let kcolors = Row::new()
+            .padding(PADDING)
             .spacing(10)
+            .push(Text::new("Colors").width(Length::Units(name_width)))
+            .push(
+                Slider::new(
+                    &mut self.kcolors_slider,
+                    1..=64,
+                    self.kcolors,
+                    Event::SliderKcolorsChanged,
+                )
+                .on_release(Event::SliderKcolorsReleased)
+                .width(Length::Fill)
+                .style(self.theme),
+            )
+            .push(
+                Text::new(self.kcolors.to_string())
+                    .width(Length::Units(val_width))
+                    .font(FONT_PIX_L),
+            );
+
+        let level_black = Row::new()
+            .spacing(10)
+            .push(Text::new("black").width(Length::Units(name_width)))
+            .push(
+                Slider::new(
+                    &mut self.level_black_slider,
+                    0..=100,
+                    self.level_black,
+                    Event::SliderLevelBlackChanged,
+                )
+                .on_release(Event::SliderLevelBlackReleased)
+                .width(Length::Fill)
+                .style(self.theme),
+            )
+            .push(
+                Text::new(format!("{} %", self.level_black))
+                    .width(Length::Units(val_width))
+                    .font(FONT_PIX_L),
+            );
+
+        let level_white = Row::new()
+            .spacing(10)
+            .push(Text::new("white").width(Length::Units(name_width)))
+            .push(
+                Slider::new(
+                    &mut self.level_white_slider,
+                    0..=100,
+                    self.level_white,
+                    Event::SliderLevelWhiteChanged,
+                )
+                .on_release(Event::SliderLevelWhiteReleased)
+                .width(Length::Fill)
+                .style(self.theme),
+            )
+            .push(
+                Text::new(format!("{} %", self.level_white))
+                    .width(Length::Units(val_width))
+                    .font(FONT_PIX_L),
+            );
+
+        let levels = Row::new()
+            .padding(PADDING)
+            .spacing(10)
+            .push(Text::new("Levels").width(Length::Units(name_width)))
+            .push(Column::new().push(level_black).push(level_white));
+
+        let content = Column::new()
+            .spacing(5)
             .align_items(Align::Center)
             .width(Length::Fill)
             .push(header)
             .push(image)
-            .push(resize);
+            .push(pixelize)
+            .push(kcolors)
+            .push(levels);
 
         let scrollable = Scrollable::new(&mut self.scroll).push(
             Container::new(content)
@@ -165,7 +274,10 @@ impl Easel {
         let Easel {
             img_path,
             img_handle,
-            resize_val,
+            pixelize,
+            kcolors,
+            level_black,
+            level_white,
             ..
         } = self;
 
@@ -174,14 +286,14 @@ impl Easel {
                 .arg("convert")
                 .arg(img_path.to_string_lossy().as_ref())
                 .arg("-resize")
-                .arg(format!("{}%", 100 - *resize_val))
+                .arg(format!("{}%", 100 - *pixelize))
                 .arg("-level")
-                .arg("10%,80%")
+                .arg(format!("{}%,{}%", level_black, level_white))
                 .arg("-")
                 | Exec::cmd("magick")
                     .arg("-")
                     .arg("-kmeans")
-                    .arg("32")
+                    .arg(kcolors.to_string())
                     .arg("-")
                 | Exec::cmd("magick")
                     .arg("convert")
@@ -189,7 +301,7 @@ impl Easel {
                     .arg("-filter")
                     .arg("point")
                     .arg("-resize")
-                    .arg(format!("{}%", 1.0 / (100 - *resize_val) as f32 * 10_000.0))
+                    .arg(format!("{}%", 1.0 / (100 - *pixelize) as f32 * 10_000.0))
                     .arg("-"))
             .capture()
             .unwrap()
